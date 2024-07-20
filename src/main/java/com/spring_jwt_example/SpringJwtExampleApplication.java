6523f1b00dfc2eae9a9958ac194f9c399a7741fe
pekellogg <peyton846@revature.net>
2024-07-19 18:26:26 -0700

    initial commit

---
+++ src/main/java/com/spring_jwt_example/SpringJwtExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring_jwt_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_jwt_example/SpringJwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_jwt_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
